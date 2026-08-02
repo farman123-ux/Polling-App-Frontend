eb8ee0eb14704219e53a46daed240d63cc44fd31
farman123-ux <fk160061@gmail.com>
2026-08-02 11:05:09 -0700

    Same changing Polling code for frontend

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-//to create a single instance 
-const api=axios.create({baseURL:"/api"});
+// to create a single instance
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+});
 
-//to attach jwt token to any request //check user logedin or not 
-api.interceptors.request.use((config)=>{
-      const token =localStorage.getItem("token");
-      if(token) config.headers.Authorization = `Bearer ${token}`;
-      return config;
-
+// to attach jwt token to any request //check user logged in or not
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default api;
